660965fa29ff7ff13631c5a4b76fa7f92aa71c48
Lemmuel Alinea <lemmuelrodge22@gmail.com>
2026-02-24 07:40:11 +0800

    /api to auth

--- dms-frontend/src/pages/Login.jsx
+++ dms-frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("/auth/login", {
+      const res = await axios.post("/api/auth/login", {
         email,
         password,
       });
