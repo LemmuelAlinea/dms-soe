89bd163bc0ca001706dcbce52a079a33cb662c2a
Lemmuel Alinea <lemmuelrodge22@gmail.com>
2026-02-24 06:29:30 +0800

    Update frontend API to production URL

--- dms-frontend/src/services/api.js
+++ dms-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://dms-soe-production.up.railway.app/",
 });
 
 api.interceptors.request.use((config) => {
