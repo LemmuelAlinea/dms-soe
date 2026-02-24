c5aaa6ec16c8651bf4a11c99348cb9f66cb315d2
Lemmuel Alinea <lemmuelrodge22@gmail.com>
2026-02-24 08:03:48 +0800

    a

--- dms-frontend/src/api/axios.js
+++ dms-frontend/src/api/axios.js
@@ -5,7 +5,7 @@ export default axios.create({
   baseURL: "https://dms-soe-production.up.railway.app",
 });
 
-instance.interceptors.request.use((config) => {
+axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
