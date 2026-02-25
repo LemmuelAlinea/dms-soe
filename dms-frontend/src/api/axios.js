e7b89e9686706ec0a8cd86c494e63ad348b48803
Lemmuel Alinea <lemmuelrodge22@gmail.com>
2026-02-26 02:31:30 +0800

    Removed /api in axios.js

--- dms-frontend/src/api/axios.js
+++ dms-frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://dms-soe-production.up.railway.app/api",
+  baseURL: "https://dms-soe-production.up.railway.app",
 });
 
 instance.interceptors.request.use((config) => {
